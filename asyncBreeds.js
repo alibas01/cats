2b7789d7c58a364ef2fba15e40b39d99968a790c
Ali Bas <alibas01@gmail.com>
2021-01-12 21:36:53 -0500

    mocha tests added

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,13 +6,15 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     
-    if (!error) callback(data);
+    //if (!error) 
+    callback(data);
+    
   });
   
 };
 
 // we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', breed => { 
-  console.log('Return Value: ', breed)
-});
+
+
+module.exports = breedDetailsFromFile;
 
